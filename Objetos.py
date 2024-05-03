a7c8e5615bac5f0e0cd9ab580e044873126297a1
keyteer <pepalacios2021@udec.cl>
2024-05-03 15:00:53 -0400

    CrearObjetos

--- Objetos.py
+++ Objetos.py
@@ -1,4 +1,5 @@
 import random
+import math
 import pygame
 
 import Global
@@ -14,7 +15,7 @@ class Bloque(pygame.sprite.Sprite):
 
 
 class Objeto(pygame.sprite.Group):
-    def __init__(self, id, peso, valor, volumen, color=None):
+    def __init__(self, id, peso, valor, volumen, tmno_max=(1000, 1000), color=None):
         super().__init__()
         self.id = id
         self.peso = peso  # Valor numérico.
@@ -23,20 +24,25 @@ class Objeto(pygame.sprite.Group):
             color = (random.randint(100, 255),
                      random.randint(100, 255),
                      random.randint(100, 255))
-        matriz = self.ConstruirForma(volumen)  # Crear la forma con volumen cuadraditos.
+        matriz = self.ConstruirForma(volumen, tmno_max)  # Crear la forma con volumen cuadraditos.
         self.crear_bloques(matriz, color)  # Crear los bloques del objeto.
 
-    def ConstruirForma(self, volumen):
-        lista_cuadrados = []
-        lista_posibles_cuadrados = [(0, 0)]
+    def ConstruirForma(self, volumen, tmno_max):
+        tmno_max = (tmno_max[0] // 2, tmno_max[1] // 2)  # dividir por 2, más facil de manejar
+        lista_cuadrados = []  # Lista de cuadrados que forman el objeto.
+        lista_posibles_cuadrados = [(0, 0)]  # Lista de cuadrados que pueden ser añadidos.
         for _ in range(volumen):
+            # Elegir un cuadrado aleatorio.
             nuevo_cuadrado = random.choice(lista_posibles_cuadrados)
             lista_cuadrados.append(nuevo_cuadrado)
             while nuevo_cuadrado in lista_posibles_cuadrados:
                 lista_posibles_cuadrados.remove(nuevo_cuadrado)
+            # Añadir los cuadrados adyacentes al nuevo cuadrado.
             for c in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 vecino = (nuevo_cuadrado[0] + c[0], nuevo_cuadrado[1] + c[1])
-                if vecino not in lista_cuadrados:
+                if (vecino not in lista_cuadrados
+                        and -tmno_max[0] <= vecino[0] <= tmno_max[0]
+                        and -tmno_max[1] <= vecino[1] <= tmno_max[1]):
                     lista_posibles_cuadrados.append(vecino)
 
         # Naturalizar la lista de cuadrados.
@@ -50,6 +56,28 @@ class Objeto(pygame.sprite.Group):
         for i in range(len(cuadrados)):
             Bloque(cuadrados[i][0], cuadrados[i][1], color, self)
 
+    # tamaño del objeto
+    def __get_width(self):
+        return (max([bloque.rect.x for bloque in self.sprites()])
+                - min([bloque.rect.x for bloque in self.sprites()]))
+
+    def __get_height(self):
+        return (max([bloque.rect.y for bloque in self.sprites()])
+                - min([bloque.rect.y for bloque in self.sprites()]))
+
+    def get_size(self):
+        return self.__get_width(), self.__get_height()
+
+    # posicion global del objeto
+    def __get_x(self):
+        return min([bloque.rect.x for bloque in self.sprites()])
+
+    def __get_y(self):
+        return min([bloque.rect.y for bloque in self.sprites()])
+
+    def get_pos(self):
+        return self.__get_x(), self.__get_y()
+
     def mover(self, x, y):
         for bloque in self.sprites():
             bloque.rect.x += x * Global.tmno_cuad
@@ -62,27 +90,33 @@ class Objeto(pygame.sprite.Group):
             bloque.rect.topleft = bloque0[0] + bloque0[1] - y, bloque0[1] - bloque0[0] + x
 
 
+def CrearObjetos(tmno_max, n_objetos):
+    volums = [math.ceil(random.triangular(1,tmno_max[0]*tmno_max[1]/16,4)) for _ in range(n_objetos)]
+    print(tmno_max[0]*tmno_max[1]/4)
+    print(volums)
+
+    objetos = []
+    for i in range(n_objetos):
+        objetos.append(Objeto(i, 1, 1, volums[i], tmno_max))
+
+    return objetos
 
 
-
-# def CrearObjetos(mochila):
-#    id, i = 1, 0
-#    ConjObjetos = []
-#    while i <= mochila.tamano[0] * mochila.tamano[1]:
-#        razonTamano = random.randint(2, mochila.tamano[0] - 1), random.randint(2, mochila.tamano[1] - 1)
-#        razonValor = random.randint(1, 100)
-#        ConjObjetos.append(Objeto(id, None, razonTamano, razonValor))
-#        i += ConjObjetos[id - 1].cuadraditos
-#        id += 1
-#    for objeto in ConjObjetos:
-#        objeto.peso = (mochila.peso // len(ConjObjetos)) + random.randint(-mochila.peso // 5, mochila.peso // 5)
-#    return ConjObjetos
-
-
+#################### TEST ####################
 def main():
-    objeto = Objeto(1, 1, (5, 5), 9)
-    for fila in objeto.matriz:
-        print(fila)
+    WIDTH, HEIGHT = 400, 400
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('generator test')
+    objs = CrearObjetos((20, 20), 100)
+
+    sum = 0
+    for obj in objs:
+        obj.mover(sum,0)
+        sum += obj.get_size()[0] //Global.tmno_cuad + 1
+        obj.draw(screen)
+    pygame.display.flip()
+    pygame.time.wait(10000) #
 
 
 if __name__ == "__main__":
