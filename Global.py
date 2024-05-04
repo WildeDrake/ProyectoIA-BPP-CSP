8505278d414fb31c24b1f637f9511e0a16a0b6d2
dievenegas <diegovenegasanabalon@gmail.com>
2024-05-04 02:32:16 -0400

    Complejización de tamCasillas

--- Global.py
+++ Global.py
@@ -1,5 +1,14 @@
-WIDTH, HEIGHT = 1000, 1000  # tamaño de la pantalla.
-dimContenedor = 100, 100    # tamaño de la contenedor.
+WIDTH, HEIGHT = 1800, 1000  # tamaño de la pantalla.
+dimContenedor = 10, 10    # tamaño de la contenedor.
 tmno_cuad = 20  # tamaño en pixeles de las cuadriculas.
-tamCasillas = min([WIDTH // (dimContenedor[0] * 2) + 1,
-                   (HEIGHT // (dimContenedor[1] * 1.1)) + 1])
+
+if min(WIDTH, HEIGHT) == WIDTH:
+    if min(dimContenedor[0], dimContenedor[1]) == dimContenedor[0]:
+        tamCasillas = WIDTH//(dimContenedor[1]*2) + 1
+    else:
+        tamCasillas = (WIDTH//(dimContenedor[0]*2)) + 1
+else:
+    if min(dimContenedor[0], dimContenedor[1]) == dimContenedor[0]:
+        tamCasillas = (HEIGHT//(dimContenedor[1]*1.1)) + 1
+    else:
+        tamCasillas = (HEIGHT//(dimContenedor[0]*1.1)) + 1
