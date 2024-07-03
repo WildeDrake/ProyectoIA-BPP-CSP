2afb26f291f3492d5a9baf1d24888611b1144c19
keyteer <pepalacios2021@udec.cl>
2024-07-03 01:21:10 -0400

    template para heuristicas

--- Agente.py
+++ Agente.py
@@ -1,3 +1,4 @@
+import SimulatedAnnealing
 import Contenedor
 import Objetos
 import Global
@@ -6,19 +7,14 @@ import Global
 contenedor = Contenedor.Contenedor(Global.dimContenedor)
 ConjObjetos = Objetos.CrearObjetos(Global.dimContenedor, 16)
 
+# Las heuristicas reciben una lista de objetos en un orden y los colocan uno a uno en el contenedor.
+def heuristica1(cont: Contenedor.Contenedor, objs: list):
+    for obj in objs:
+        pass
 
-def heuristica1():
-    pass
 
-
-# Agente 1
-esquinas = []
-def Agente1():
+# Agente
+def Agente():
     global contenedor
     global ConjObjetos
-    global esquinas
-
-
-
-
 
